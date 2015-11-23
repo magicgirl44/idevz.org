4906a13999fd873c5e6f5f624e393396a5ceabfe
idevz <15134559390@163.com>
2015-11-23 23:12:30 +0800

    nginx conf

--- config/nginx.lua
+++ config/nginx.lua
@@ -2,18 +2,19 @@ local ngx_conf = {}
 
 ngx_conf.common = {
     INIT_BY_LUA = 'nginx.init',
+    LUA_SHARED_DICT = 'nginx.sh_dict',
     CONTENT_BY_LUA_FILE = './pub/index.lua'
 }
 
 ngx_conf.env = {}
 ngx_conf.env.development = {
     LUA_CODE_CACHE = false,
-    PORT = 7200
+    PORT = 9110
 }
 
 ngx_conf.env.test = {
     LUA_CODE_CACHE = true,
-    PORT = 7201
+    PORT = 9111
 }
 
 ngx_conf.env.production = {
